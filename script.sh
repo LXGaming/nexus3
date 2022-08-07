0f963367bc944d44564d3b8c3008e3411dd868c5
Alex Thomson <LXGaming@users.noreply.github.com>
2022-08-07 15:18:51 +1200

    Always prefix version

--- script.sh
+++ script.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
 currentVersion=$(git describe --tags --abbrev=0)
-latestVersion=$(git -c "versionsort.suffix=-" \
+latestVersion=v$(git -c "versionsort.suffix=-" \
     ls-remote --refs --sort="version:refname" --tags https://github.com/sonatype/docker-nexus3 \
     | tail --lines=1 \
     | cut --delimiter="/" --fields=3)
-
-if [ "$latestVersion" != \v* ]; then
-  latestVersion="v$latestVersion"
-fi
 
 echo "Current Version: $currentVersion"
 echo "Latest Version: $latestVersion"
